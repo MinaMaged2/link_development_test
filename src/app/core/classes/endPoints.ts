5248c8dcf3675f17a2d68e7520edbf73ceec0c9e
MinaMaged2 <mena.maged949@gmail.com>
2024-12-14 16:49:21 +0200

    add the layout

---
+++ src/app/core/classes/endPoints.ts
@@ -0,0 +1,7 @@
+
+import { environment } from '../../../environments/environment.development';
+
+export class EndPoints {
+  static readonly Slides: string = environment.API + 'fee177346e7875554413';
+
+}
