052c18d0a93259565662b23da960821352bf6e1d
MinaMaged2 <mena.maged949@gmail.com>
2024-12-15 17:08:18 +0200

    finish things we do

---
+++ src/app/features/home/components/things-we-do/things-we-do.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-things-we-do',
+  standalone: true,
+  imports: [],
+  templateUrl: './things-we-do.component.html',
+  styleUrl: './things-we-do.component.scss'
+})
+export class ThingsWeDoComponent implements OnInit{
+
+  cars: any[] = [
+    {
+      itemTitle: "Crafty Mind",
+      image: "assets/imgs/things-we-do-3.png",
+      url: "/"
+    },
+    {
+      itemTitle: "Services",
+      image: "assets/imgs/things-we-do-4.png",
+      url: "/"
+    },
+    {
+      itemTitle: "Envision",
+      image: "assets/imgs/things-we-do-1.png",
+      url: "/"
+    },
+    {
+      itemTitle: "Envision",
+      image: "assets/imgs/things-we-do-2.png",
+      url: "/"
+    },
+    {
+      itemTitle: "Transformation",
+      image: "assets/imgs/things-we-do-3.png",
+      url: "/"
+    }
+  ]
+
+  groupedItems: Array<any[]> = [];
+
+  ngOnInit() {
+    this.groupedItems = this.groupArray(this.cars, 2); // Group into pairs
+  }
+
+  groupArray(arr: any[], groupSize: number): Array<any[]> {
+    const grouped = [];
+    for (let i = 0; i < arr.length; i += groupSize) {
+      grouped.push(arr.slice(i, i + groupSize));
+    }
+    return grouped;
+  }
+}
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,32 +1,36 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MainBannerComponent } from './components/main-banner/main-banner.component';
 import { HomeService } from './services/home.service';
 import { Banner } from '../../core/models/banner';
-import { BannerItemComponent } from './components/banner-item/banner-item.component';
+import { ThingsWeDoComponent } from './components/things-we-do/things-we-do.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainBannerComponent],
+  imports: [MainBannerComponent, ThingsWeDoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
   private homeService = inject(HomeService);
 
-  bannersData!: Banner[]; 
+  bannersData!: Banner[];
 
   ngOnInit(): void {
     this.getBannersData();
   }
 
-  getBannersData(){
+  getBannersData() {
     this.homeService.getBanners().subscribe({
       next: (res) => {
         this.bannersData = res.slides;
-      }
-    })
+      },
+    });
   }
 }
